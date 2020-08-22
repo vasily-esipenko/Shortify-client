b77cdcdc9149cc2fac128388412c6311efb9363f
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-22 22:34:29 +0300

    Update Links.vue

---
+++ src/views/Links.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>Links page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Links'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>>
