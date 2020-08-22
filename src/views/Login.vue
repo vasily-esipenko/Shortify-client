dfceba78aca9ca3da8cf2d5f52dc9cffba95ef7b
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-22 22:34:58 +0300

    Update Login.vue

---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>Login page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
