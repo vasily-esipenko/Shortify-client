00ecfb3c12fba3788209a93d8d0bf319ac2eb946
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-22 22:35:19 +0300

    Update Signup.vue

---
+++ src/views/Signup.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>Signup page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Signup'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
