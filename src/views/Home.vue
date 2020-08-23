92fe55f53dd83a7949a3b60e624a4bd0465ac438
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-23 11:07:09 +0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="continer">
         <h1>Home page</h1>
+        <LinkForm />
     </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import LinkForm from '@/components/LinkForm.vue';
 
 @Options({
   components: {
+      LinkForm,
   },
 })
 export default class Home extends Vue {}
