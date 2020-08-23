82336c610859aa3c41a284de27b6b1cf3ba70c91
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-23 11:05:51 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
+    <Navbar />
     <router-view/>
+    <Footer />
   </div>
 </template>
 
+<script>
+import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+    name: 'App',
+    components: {
+        Navbar,
+        Footer,
+    }
+}
+</script>
+
 <style lang="scss">
+@import '@/styles/_variables.scss';
+@import '@/styles/_mixins.scss';
+
+body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+}
+
+a, a:hover {
+    text-decoration: none;
+    color: $secondaryTextColor;
+}
+
+*::before,
+*::after {
+    box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
