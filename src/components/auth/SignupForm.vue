1cd88f22aa0d6eb08fb0f1e83fec6ac105888d46
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-26 20:47:23 +0300

    Add SignupForm.vue

---
+++ src/components/auth/SignupForm.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="signup-form">
+        <h1>Sign up</h1>
+        <form class="form">
+            <div class="inputs col">
+
+                <div class="form-row">
+                    <div class="col">
+                        <label class="sr-only" for="email">email</label>
+                        <input type="text" class="form-control" id="email" placeholder="email" v-model="userForm.email">
+                    </div>
+                </div>
+
+                <div class="form-row">
+                    <div class="col">
+                        <label class="sr-only" for="password">password</label>
+                        <input type="text" class="form-control" id="password" placeholder="password" v-model="userForm.password">
+                    </div>
+                </div>
+
+                <div class="form-row">
+                    <div class="col">
+                        <label class="sr-only" for="confirm-password">confirm password</label>
+                        <input type="text" class="form-control" id="confirm-password" placeholder="confirm password" v-model="confirmPassword">
+                    </div>
+                </div>
+
+            </div>
+
+            <button type="submit" class="btn btn-primary" @click.prevent="signup">Sign up</button>
+        </form>
+    </div>
+</template>
+
+<script lang="ts">
+
+export default {
+    name: 'SignupForm',
+    data() {
+        return {
+            userForm: {
+                email: "",
+                password: "",
+            },
+            confirmPassword: "",
+            alertMessage: ""
+        };
+    },
+    methods: {
+        signup() {
+            console.log(this.userForm);
+        }
+    }
+};
+
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/_mixins.scss';
+
+.signup-form {
+    @include authForm();
+}
+
+.inputs {
+    @include authInput();
+
+    .form-row {
+        margin: .3rem auto;
+    }
+}
+</style>>
