237321a6cccdfaaa07b923cd3067c016f3b21772
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-27 20:55:34 +0300

    Update auth.ts

--- src/store/modules/auth.ts
+++ src/store/modules/auth.ts
@@ -1,7 +1,56 @@
 export default {
-    action: {
+    actions: {
+        async signup(ctx: any, user: object) {
+            const res = await fetch('http://localhost:1337', {
+                method: "POST",
+                body: JSON.stringify(user),
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                }
+            });
+
+            const response = await res.json();
+
+            if (localStorage.getItem("token")) localStorage.token = response.token;
+            else localStorage.setItem("token", JSON.stringify(response.token));
+
+            ctx.commit('setAuthData', response.message);
+        },
+
+        async login(ctx: any, user: object) {
+            const res = await fetch('http://localhost:1337', {
+                method: "POST",
+                body: JSON.stringify(user),
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                }
+            });
+
+            const response = await res.json();
+
+            if (localStorage.getItem("token")) localStorage.token = response.token;
+            else localStorage.setItem("token", JSON.stringify(response.token));
+
+            ctx.commit('setAuthData', response.message);
+        },
+
+        async verifyUser(ctx: any, token: string) {
+            console.log('Verify user function');
+        }
     },
-    mutations: {},
-    state: {},
-    getters: {}
+    mutations: {
+        setAuthData(state: any, data: string) {
+            state.authData = data;
+        }
+    },
+    state: {
+        authData: ""
+    },
+    getters: {
+        getAuthData(state: any) {
+            return state.authData;
+        }
+    }
 };
